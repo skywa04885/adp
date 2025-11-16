cf4f402fa7cc26dd79914fbc8c914dbd0b7b86c8
Luke Rieff <luke.rieff@gmail.com>
2025-11-16 18:40:38 +0100

    Add sorting benchmarks

---
+++ src/benchmark/java/com/github/skywa04885/MyMergeSortBenchmark.java
@@ -0,0 +1,55 @@
+package com.github.skywa04885;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 10, time = 1)
+@Fork(2)
+public class MyMergeSortBenchmark {
+    @State(Scope.Thread)
+    public static class BenchmarkState {
+        @Param({"1000", "10000", "50000"})
+        int size;
+
+        Integer[] unsorted;
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            unsorted = generateRandomArray(size);
+        }
+
+        private Integer[] generateRandomArray(final int size) {
+            final Random random = new Random(12345);
+            final Integer[] arr = new Integer[size];
+
+            for (int i = 0; i < size; i++) {
+                arr[i] = random.nextInt();
+            }
+
+            return arr;
+        }
+    }
+
+    @Benchmark
+    public Integer[] sort(BenchmarkState state) {
+        return MyMergeSort.getInstance().sort(state.unsorted);
+    }
+
+    public static void main(final String[] args) throws RunnerException {
+        final Options options = new OptionsBuilder()
+                .include(MyMergeSortBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(options).run();
+    }
+}
